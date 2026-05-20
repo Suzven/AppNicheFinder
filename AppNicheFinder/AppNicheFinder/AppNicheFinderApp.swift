e88f52f560b1c49877a3ae7787b78626b4d71853
Suzven <nevzus@gmail.com>
2026-05-20 18:51:11 +0300

    added base project

---
+++ AppNicheFinder/AppNicheFinder/AppNicheFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppNicheFinderApp.swift
+//  AppNicheFinder
+//
+//  Created by Алексей Коваленко on 20.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct AppNicheFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
